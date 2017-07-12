7f77d26ad6d5805c4041d12221179863f352909a
Roberto Villarejo Martinez <robertovillarejo@outlook.com>
2017-07-12 14:26:37 -0500

    Singularize retorna la forma en singular no el lema

--- src/main/java/mx/infotec/inflector/Inflector.java
+++ src/main/java/mx/infotec/inflector/Inflector.java
@@ -1,5 +1,4 @@
 package mx.infotec.inflector;
-import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeMap;
@@ -20,17 +19,17 @@ public class Inflector {
 	private final String lang;
 
 	/**
-	 *
+	 * 
 	 * @param data The Freeling data folder
 	 * @param lang The language in ISO639-1 format
 	 */
 	public Inflector(String data, String lang) {
 		this.lang = lang;
 		this.dict = new Dictionary(
-				lang,
-				data + lang + File.separatorChar + "dicc.src",
-				true,
-				data + lang + File.separatorChar + "afixos.dat",
+				lang, 
+				data + lang + "\\dicc_nouns.src", 
+				true, 
+				data + lang + "\\afixos.dat", 
 				true,
 				true);
 	}
@@ -54,14 +53,7 @@ public class Inflector {
 	 * @return the response
 	 */
 	public Response singularize(String word) {
-		Response response = new Response(word, lang);
-		TreeMap<String, String> lemmaTag = this.searchOnDictionary(word);
-
-		if (lemmaTag.size() > 0) {
-			response.setFound(true);
-			response.setResult(lemmaTag.keySet());
-		}
-		return response;
+		return this.process(word, 'S');
 	}
 
 	/**
@@ -70,34 +62,40 @@ public class Inflector {
 	 * @return the response
 	 */
 	public Response pluralize (String word) {
+		return this.process(word, 'P');
+	}
+	
+	private Response process(String word, char number) {
+		
 		Response response = new Response(word, lang);
 		TreeMap<String, String> lemmaTag = this.searchOnDictionary(word);
 
 		Set<String> lemas = lemmaTag.keySet();
-		Set<String> plurals = new HashSet<>();
+		Set<String> singular = new HashSet<>();
 		for (String lemma : lemas) {
 			if (lemmaTag.get(lemma).length() > 2) {	// Ignores some tags
 				char genre = lemmaTag.get(lemma).charAt(2);	// Can be 'M' or 'F'
 
-				String pluralTag = "NC" + genre + "P000";
-				String invariableTag = "NC" + genre + "N000";
+				String pluralTag = "NC" + genre + number + "000";
+				String invariableTag = "NC" + genre + number + "000";
 
 				ListString forms = dict.getForms(lemma, pluralTag);
-				ListString formsNeutral = dict.getForms(lemma, invariableTag);
+				ListString formsNeutral = dict.getForms(lemma, invariableTag);	
+
 				if (!formsNeutral.empty()) {
 					forms.pushBack(formsNeutral.front());	// Invariable words only return one result
-				}
+				}			
 
 				while (!forms.empty()) {
 					if (!"".equals(forms.front())) {
-						plurals.add(forms.front());
-					}
+						singular.add(forms.front());
+					}					
 					forms.popFront();
 				}
 			}
 
 		}
-		response.setResult(plurals);
+		response.setResult(singular);
 		return response;
 	}
 
