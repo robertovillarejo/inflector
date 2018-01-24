cf814b22a91498afe8201da3a7d74b9db35f90a8
Roberto Villarejo Martínez <robertovillarejo@outlook.com>
2018-01-23 21:05:54 -0600

    Autowiring Inflectors

--- src/main/java/mx/infotec/inflector/web/InflectorController.java
+++ src/main/java/mx/infotec/inflector/web/InflectorController.java
@@ -1,11 +1,8 @@
 package mx.infotec.inflector.web;
 
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,14 +18,12 @@ import mx.infotec.inflector.engine.SpanishInflector;
 @RestController
 @RequestMapping("/api")
 public class InflectorController {
-	private SpanishInflector spanishInflector;
-	private EnglishInflector englishInflector;
-	private List<String> langsSupported = Arrays.asList("es", "en");
 	
-	public InflectorController(SpanishInflector spanishInflector, EnglishInflector englishInflector) {
-		this.spanishInflector = spanishInflector;
-		this.englishInflector = englishInflector;
-	}
+	@Autowired
+	private SpanishInflector spanishInflector;
+	
+	@Autowired
+	private EnglishInflector englishInflector;
 
 	@GetMapping(path="/es/singularize")
 	public Response esSingularize(@RequestParam String word) {
@@ -79,11 +74,6 @@ public class InflectorController {
 		if (result != null) response.isFound(true);
 		response.setResult(result);
 		return response;
-	}
-	
-	@RequestMapping(method = RequestMethod.GET, path = "/languages")
-	public List<String> getLangsSupported() {
-		return this.langsSupported;
 	}
 
 }
